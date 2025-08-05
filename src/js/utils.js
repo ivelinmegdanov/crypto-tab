43a8b7f893c871dd4ebc0430a9079c22ad377029
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2025-08-05 10:39:32 +0300

    Price format refactor

--- src/js/utils.js
+++ src/js/utils.js
@@ -6,7 +6,16 @@ window.App.Utils = {
      * https://stackoverflow.com/a/14467460/1333836
      */
     formatPrice(_p) {
-        const price = _p.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        const priceStr = _p.toString();
+        
+        // Split by decimal point
+        const parts = priceStr.split('.');
+        
+        // Add commas only to the integer part (before decimal)
+        const integerPart = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+        
+        // Reconstruct with decimal part if it exists
+        const price = parts.length > 1 ? `${integerPart}.${parts[1]}` : integerPart;
 
         return `$${price}`;
     }
